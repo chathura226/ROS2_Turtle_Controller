cc124ae6578a65ea994679f9d62bfce8f550b3b3
chathura226 <chathuralakshan226@gmail.com>
2024-05-20 14:09:32 +0530

    working with services and wrting a client using python

--- NOTE.md
+++ NOTE.md
@@ -7,5 +7,16 @@ rqt_graph
 ros2 topic list
 ros2 node list
 ros2 topic info <topic>
+ros2 topic hz <topic>
 ros2 node info <node>
 ros2 interface show <data type of topic >
+
+
+ros topics are mainly for publisher subscriber pattern
+
+to make a request and et a response like in client server , we can use ros services
+
+in rqt_graph , we cannot see services
+
+we can make req to services using cmd:
+ros2 service call /turtle1/set_pen turtlesim/srv/SetPen "{'r':0,'g':0,'b':0,'width':1,'off':0}"
--- src/my_robot_controller/my_robot_controller/turtle_controller.py
+++ src/my_robot_controller/my_robot_controller/turtle_controller.py
@@ -3,7 +3,9 @@ import rclpy
 from rclpy.node import Node
 from turtlesim.msg import Pose
 from geometry_msgs.msg import Twist
- 
+from turtlesim.srv import SetPen
+from functools import partial
+
 class TurtleControllerNode(Node):
 
     def __init__(self):
@@ -14,6 +16,14 @@ class TurtleControllerNode(Node):
 
     def pose_callback(self,pose:Pose):
         msg=Twist()
+        
+        if pose.x>5.5:
+            self.get_logger().info("Changing pen color to red")
+            self.call_set_pen_service(255,0,0,10,0) 
+        else:
+            self.get_logger().info("Changing pen color to green")
+            self.call_set_pen_service(0,255,0,5,0)    
+
         if pose.x>8.0 or pose.x<2.0 or pose.y>8.0 or pose.y<2.0:
             msg.linear.x=1.0
             msg.angular.z=0.9 
@@ -23,6 +33,27 @@ class TurtleControllerNode(Node):
         msg.linear.x=5.0
         msg.angular.z=0.0
         self.cmd_vel_publisher.publish(msg)
+
+    def call_set_pen_service(self,r,g,b,width,off):
+        client=self.create_client(SetPen,'/turtle1/set_pen')
+        while not client.wait_for_service(1.0):
+            self.get_logger().warn("Waiting for service")
+        
+        request=SetPen.Request()
+        request.r=r
+        request.g=g
+        request.b=b
+        request.width=width
+        request.off=off
+
+        future=client.call_async(request)
+        future.add_done_callback(partial(self.callback_set_pen))
+
+    def callback_set_pen(self,future):
+        try:
+            response=future.result()
+        except Exception as e:
+            self.get_logger().error("Service call failed : %r" % (e,))
 
 def main(args=None):
     rclpy.init(args=args)

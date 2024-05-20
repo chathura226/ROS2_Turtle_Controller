293a53e7d98b53cddbc8bedb73e062e39b48675a
chathura226 <chathuralakshan226@gmail.com>
2024-05-20 14:14:36 +0530

    adjusted freq of service req

--- src/my_robot_controller/my_robot_controller/turtle_controller.py
+++ src/my_robot_controller/my_robot_controller/turtle_controller.py
@@ -10,6 +10,7 @@ class TurtleControllerNode(Node):
 
     def __init__(self):
         super().__init__("turtle_controller")
+        self.prev_x_=0.0
         self.cmd_vel_publisher=self.create_publisher(Twist,"/turtle1/cmd_vel",10)
         self.pose_subscriber_=self.create_subscription(Pose,"/turtle1/pose",self.pose_callback,10)
         self.get_logger().info("Turtle Controller has been started!")
@@ -17,10 +18,10 @@ class TurtleControllerNode(Node):
     def pose_callback(self,pose:Pose):
         msg=Twist()
         
-        if pose.x>5.5:
+        if pose.x>5.5 and self.prev_x_<=5.5:
             self.get_logger().info("Changing pen color to red")
             self.call_set_pen_service(255,0,0,10,0) 
-        else:
+        elif pose.x<=5.5 and self.prev_x_>5.5:
             self.get_logger().info("Changing pen color to green")
             self.call_set_pen_service(0,255,0,5,0)    
 
@@ -33,6 +34,7 @@ class TurtleControllerNode(Node):
         msg.linear.x=5.0
         msg.angular.z=0.0
         self.cmd_vel_publisher.publish(msg)
+        self.prev_x_=pose.x
 
     def call_set_pen_service(self,r,g,b,width,off):
         client=self.create_client(SetPen,'/turtle1/set_pen')
